b5e70587644c08c4e6fc15774e343a42f1845788
webmasterslava <webmasterslava@gmail.com>
2012-05-24 18:23:12 +0700

    toolbar updated

--- themes/myplanet/templates/html.tpl.php
+++ themes/myplanet/templates/html.tpl.php
@@ -8,6 +8,8 @@
   <?php print $scripts; ?>
 </head>
 <body class="<?php print $classes; ?>" <?php print $attributes;?>>
+  <?php print $page_top; ?>
   <?php print $page; ?>
+  <?php print $page_bottom; ?>
 </body>
 </html>
