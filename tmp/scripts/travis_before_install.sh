8ad7428c967fe2dbe4f2178159bbb6cdd7957034
Patrick Connolly <patrick.c.connolly@gmail.com>
2012-11-02 12:36:42 -0400

    Fixed PROJECT_NAME envvar and checking for casperjs.

--- tmp/scripts/travis_before_install.sh
+++ tmp/scripts/travis_before_install.sh
@@ -2,7 +2,9 @@
 # Travis Testing Script for CI Testing
 
 # Run Headless Testing Server
-phantomjs --version
+echo "PhantomJS version: `phantomjs --version`"
 export PHANTOMJS_EXECUTABLE='phantomjs --local-to-remote-url-access=yes --ignore-ssl-errors=yes'
 export DISPLAY=:99.0
 sh -e /etc/init.d/xvfb start
+sleep 5
+echo "CasperJS version: `casperjs --version`"
