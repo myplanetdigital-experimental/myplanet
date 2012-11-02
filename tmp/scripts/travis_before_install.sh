590e4d0dcf29469fe2600d032adc3aef29e2ad17
Patrick Connolly <patrick.c.connolly@gmail.com>
2012-11-02 16:56:02 -0400

    Trying to get casperjs working.

--- tmp/scripts/travis_before_install.sh
+++ tmp/scripts/travis_before_install.sh
@@ -1,13 +1,10 @@
 #!/bin/sh
 # Travis Testing Script for CI Testing
 
-# Run Headless Testing Server
+# Prepare PhantomJS
 echo "PhantomJS version: `phantomjs --version`"
-export PHANTOMJS_EXECUTABLE='phantomjs --local-to-remote-url-access=yes --ignore-ssl-errors=yes'
-export DISPLAY=:99.0
 sh -e /etc/init.d/xvfb start
 
 # Install CasperJS Testing Suite
 cd ..
-pwd
-git clone git://github.com/n1k0/casperjs.git --branch=tags/${CASPERJS_VERSION}
+git clone git://github.com/n1k0/casperjs.git --branch=${CASPERJS_VERSION}
