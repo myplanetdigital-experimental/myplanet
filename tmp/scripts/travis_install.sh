ba11572167ed5f9c7f39e487dbffe1bd66a03cb8
Patrick Connolly <patrick.c.connolly@gmail.com>
2012-11-04 18:11:42 -0500

    Fixed sendmail disabling.

--- tmp/scripts/travis_install.sh
+++ tmp/scripts/travis_install.sh
@@ -16,7 +16,7 @@ mv ${PROJECT_NAME} profile
 cd $WORKSPACE/profile
 drush make build-${PROJECT_NAME}.make $WORKSPACE/build --yes
 cd $WORKSPACE/build
-php -d sendmail_path=/dev/null `which drush` si ${PROJECT_NAME} \
+php -d sendmail_path=/dev/null /usr/bin/drush si ${PROJECT_NAME} \
   --db-url=mysql://root:@127.0.0.1/${PROJECT_NAME} \
   --account-name=admin \
   --account-pass=admin \
