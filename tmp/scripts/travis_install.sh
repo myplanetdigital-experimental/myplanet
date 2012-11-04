bf2e0e1ceb64d345bc2fbd1d009d0621c6bfa00d
Patrick Connolly <patrick.c.connolly@gmail.com>
2012-11-04 18:32:07 -0500

    Created drush.ini for sendmail fix.

--- tmp/scripts/travis_install.sh
+++ tmp/scripts/travis_install.sh
@@ -16,7 +16,9 @@ mv ${PROJECT_NAME} profile
 cd $WORKSPACE/profile
 drush make build-${PROJECT_NAME}.make $WORKSPACE/build --yes
 cd $WORKSPACE/build
-php -d sendmail_path=/dev/null /usr/bin/drush si ${PROJECT_NAME} \
+mkdir ~/.drush
+echo "sendmail_path=/dev/null" > ~/.drush/drush.ini
+drush si ${PROJECT_NAME} \
   --db-url=mysql://root:@127.0.0.1/${PROJECT_NAME} \
   --account-name=admin \
   --account-pass=admin \
